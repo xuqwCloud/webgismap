ea42d4868cbfbbc4c9e079ad1699b6e4aa1fc518
xuqw <2250685378@qq.com>
2021-07-31 08:33:21 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <span>ssssss</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+html,body,#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
