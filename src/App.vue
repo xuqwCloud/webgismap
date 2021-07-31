4b28be651df68b45303745cf9b75062557a5359a
xuqw <2250685378@qq.com>
2021-07-31 11:07:03 +0800

    优化首页布局，增加菜单

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,27 @@
         <el-container class="app-out-pannel">
             <el-header class="sys-header">一张图项目系统</el-header>
             <el-container class="app-content-pannel">
-                <el-aside width="200px" class="sys-menu">左侧菜单</el-aside>
+                <el-aside class="sys-menu">
+                    <el-menu
+                        default-active="1"
+                        class="el-menu-vertical-demo"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        background-color="#545c64"
+                        text-color="#fff"
+                        active-text-color="#ffd04b"
+                        :collapse="true"
+                    >
+                        <el-menu-item index="1">
+                            <i class="el-icon-monitor"></i>
+                            <span slot="title">首页大屏</span>
+                        </el-menu-item>
+                        <el-menu-item index="2">
+                            <i class="el-icon-picture-outline"></i>
+                            <span slot="title">一张图</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-aside>
                 <el-main class="sys-content">
                     <Mapview />
                 </el-main>
@@ -19,6 +39,14 @@ export default {
     name: 'App',
     components: {
         Mapview,
+    },
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
     },
 };
 </script>
@@ -37,13 +65,15 @@ body,
     height: 100%;
 }
 .sys-header {
-    background-color: #409eff;
+    background-color: #32373e;
     line-height: 60px;
     color: #fff;
     font-size: 20px;
+    font-weight: 600;
 }
 .sys-menu {
-    background-color: #c0c4cc;
+    background-color: #545c64;
+    width: 64px !important;
 }
 .sys-content {
     padding: 5px !important;
