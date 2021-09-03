5cdd5e7947b7153c24c683dda292935604b5ee8e
xuqw <2250685378@qq.com>
2021-09-04 07:05:12 +0800

    开发完成用户注册后台接口

--- node/routers/user.js
+++ node/routers/user.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var pgConfig = "postgres://postgres:webgis@localhost:5432/webgis";
 
+//获取用户信息接口
 router.get('/get', function (req, res) {
     var client = new pg.Client(pgConfig);
     client.connect(function (isErr) {
@@ -11,8 +12,8 @@ router.get('/get', function (req, res) {
             console.log('connect error:' + isErr.message);
             client.end();
             return;
-        }
-        client.query('SELECT * FROM "user" WHERE name = $1', ['admin'], function (isErr, rst) {
+        };
+        client.query('SELECT * FROM "user" WHERE name = $1', ['张三'], function (isErr, rst) {
             if (isErr) {
                 console.log('query error:' + isErr.message);
                 res.send({
@@ -27,12 +28,36 @@ router.get('/get', function (req, res) {
                 });
             }
             client.end();
-        })
-    })
+        });
+    });
 });
 
+//用户注册接口
 router.post('/insert', function (req, res) {
-    res.send('user insert');
+    var client = new pg.Client(pgConfig);
+    client.connect(function (isErr) {
+        if (isErr) {
+            console.log('connect error:' + isErr.message);
+            client.end();
+            return;
+        };
+        client.query('INSERT INTO "user" (name, psd, phone, email) VALUES ($1, $2, $3, $4);', ['test02', 'webgis', '111', '111@qq.com'], function (isErr, rst) {
+            if (isErr) {
+                console.log('query error:' + isErr.message);
+                res.send({
+                    status: 'fail',
+                    msg: 'insert error'
+                });
+            } else {
+                console.log('insert success, data is: ' + rst);
+                res.send({
+                    status: 'success',
+                    data: []
+                });
+            }
+            client.end();
+        });
+    });
 });
 
 module.exports = router;
